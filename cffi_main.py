f70189288ff2a70ed299c709fca565dccefbc21e
Igor Permyakov <permyakov.igor.a@gmail.com>
2024-03-13 17:32:54 +0100

    Format the cffi_main.py

--- cffi_main.py
+++ cffi_main.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-
 from libnfc_ffi import ffi, nfc
+
 
 ctx = ffi.new("nfc_context**")
 
-print("nfc_init"), nfc.nfc_init(ctx)
+print("nfc_init")
+nfc.nfc_init(ctx)
+
 c = ctx[0]
 conn_strings = ffi.new("nfc_connstring[10]")
-d = nfc.nfc_list_devices(c, conn_strings, 10)
-print("nfc_list_devices", d)
+
+devices = nfc.nfc_list_devices(c, conn_strings, 10)
+print(f"nfc_list_devices {devices}")
 
 device = nfc.nfc_open(c, conn_strings[0])
-print("nfc_open", device) 
+print(f"nfc_open {device}")
 
 nfc.nfc_close(device)
 print("nfc_close")
