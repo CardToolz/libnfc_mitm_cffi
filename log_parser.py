90b7cbc6bf0ad385e2b9e0ff3994db4e841c620a
Igor Permyakov <permyakov.igor.a@gmail.com>
2024-03-13 17:48:12 +0100

    Format the log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
+from argparse import ArgumentParser
 
+from nfc_helper import FrameLogger
 # from nfc_ctypes import *
 # from nfc_wrapper import *
-from nfc_helper import *
 # from NFCReplay import *
-from argparse import ArgumentParser
+
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("-f", "--filename", dest="log_fname", default=0, type=str, help="Input JSON log filename")
-    fl = FrameLogger(easy_framing=True, log_fname=parser.parse_args().log_fname)
-    print ("Log file name: %s" % fl.log_fname)
+    parser.add_argument(
+        "-f",
+        "--filename",
+        dest="log_fname",
+        default=0,
+        type=str,
+        help="Input JSON log filename"
+    )
+    arguments = parser.parse_args()
+    fl = FrameLogger(easy_framing=True,
+                     log_fname=arguments.log_fname)
+    print(f"Log file name: {fl.log_fname}")
     fl.load()
-    print ("Loaded %d frames" % fl.get_frame_list_len())
+    print(f"Loaded {fl.get_frame_list_len()} frames")
     fl.print()
+
 
 if __name__ == "__main__":
     main()
